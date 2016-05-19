8711c6080eb1d9af92b310764bbd3179a96f2907
laura <laura@10.92.244.37>
2016-05-19 14:47:05 +0200

    lol

--- OGP1516-Hillbillies/src/hillbillies/model/CubeType.java
+++ OGP1516-Hillbillies/src/hillbillies/model/CubeType.java
@@ -4,7 +4,10 @@ package hillbillies.model;
  * An enumeration of cubeTypes.
  *    In its current definition, the class distinguishes between
  *    air, rock, tree and workshop.
- * @author laura
+ *    
+ * @author Laura Vranken & Leen Van Houdt, 
+ * 			2e bach Ingenieurswetenschappen: Objectgericht Programmeren 
+ * 			link code repository: https://github.com/leenvanhoudt/OGP1516-Hillbillies
  *
  */
 public enum CubeType {
